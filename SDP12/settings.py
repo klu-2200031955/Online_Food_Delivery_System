2cd56c95ab73524f59f52989b500917db06d0d18
Samudrala Venkata Pavan Tarun Kumar <116933974+klu-2200031955@users.noreply.github.com>
2024-07-05 00:53:19 -0700

    Update settings.py

--- SDP12/settings.py
+++ SDP12/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'SDP12.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
